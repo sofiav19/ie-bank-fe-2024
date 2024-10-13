206ee61a15b1488fc2ccdf91e521472ded5178d1
sofiav19 <svitorica.ieu2022@student.ie.edu>
2024-10-13 21:52:19 +0200

    Refactor build steps for frontend workflow: Update build commands

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devtool: "source-map"
-  }
-})
+  },
+  // Define output directory based on the environment
+  outputDir: process.env.NODE_ENV === 'uat' ? 'dist-uat' : 'dist-dev',
+});
