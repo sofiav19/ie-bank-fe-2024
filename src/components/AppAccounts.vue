0b2b3144bb109e9db0a0e208c2307ab221f8004d
sofiav19 <svitorica.ieu2022@student.ie.edu>
2024-10-13 23:02:47 +0200

    update typo of country

--- src/components/AppAccounts.vue
+++ src/components/AppAccounts.vue
@@ -27,7 +27,7 @@
                 <th scope="col">Account Number</th>
                 <th scope="col">Account Balance</th>
                 <th scope="col">Account Currency</th>
-                <th scope="col">Country</th>
+                <th scope="col">Account Country</th>
                 <th scope="col">Account Status</th>
                 <th scope="col">Actions</th>
               </tr>
